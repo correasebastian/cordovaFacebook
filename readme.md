e0040db918a3ca31eef786cb09fe99b12c701527
sebastian correa morales <correa.m.sebastian@gmail.com>
2016-02-12 15:14:19 -0500

    conraets openfb working on web, nor test in device

---
+++ readme.md
@@ -0,0 +1,8 @@
+
+no instalar ngopenfb desde bower
+sino coger el archivo desde conraets 
+https://raw.githubusercontent.com/ccoenraets/OpenFB/master/ngopenfb.js , lo puse en www/ngopenfb.js
+
+
+---seguir el tutoril con este 
+https://github.com/ccoenraets/OpenFB/blob/master/indexng.html
---
+++ www/js/controllers.js
@@ -0,0 +1,63 @@
+var of;
+
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, ngFB) {
+
+    var options = {
+        scope: "email"
+    }
+    of = ngFB;
+
+    ngFB.init({
+        appId: '1021625004561044'
+    });
+
+    $scope.login = function() {
+        debugger;
+        ngFB.login(options)
+            .then(function(res) {
+                console.log('ok', res)
+            }).catch(function(error) {
+                console.log('error', error)
+            })
+    }
+
+    $scope.getInfo = function() {
+        ngFB.api({
+                path: '/me'
+            }).then(
+                function(user) {
+                    console.log(JSON.stringify(user));
+                    $scope.user = user;
+                })
+            .catch(function(error) {
+                console.log('error', error)
+            })
+    }
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+});
